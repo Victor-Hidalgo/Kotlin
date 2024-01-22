2264fe87dc5cdb566235f9944c8e1f89cd1df12a
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-01-22 09:18:49 -0400

    Add files via upload

---
+++ 2024/Variables/src/ByteShortIntLong.kt
@@ -0,0 +1,28 @@
+fun main(){
+    var number = 22
+    val maxIntegerValue = Int.MAX_VALUE
+    val minIntegerValue = Int.MIN_VALUE
+
+    println("Integer maximum value is: $maxIntegerValue")
+    println("Integer minimum value is: $minIntegerValue\n")
+
+    number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte maximum value is: $myMaxByteValue")
+    println("Byte minimum value is: $myMinByteValue\n")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+
+    println("Short maximum value is: $myMaxShortValue")
+    println("Short minimum value is: $myMinShortValue\n")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Long maximum value is: $myMaxLongValue")
+    println("Long minimum value is: $myMinLongValue\n")
+}
---
+++ 2024/Variables/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var userName = "Victor"
+    //statistically typed - checks types at compile time, not at runtime
+    userName = "Richard"
+    var age = 22
+    age = 24
+
+    println("Hello $userName! Your age is $age")
+}

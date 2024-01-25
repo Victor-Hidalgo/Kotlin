815f7a30dffc679305655385150a4e898c4754b1
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-01-25 12:58:51 -0400

    Add files via upload

---
+++ 2024/Variables/src/Operators.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    //timestamp - 1:35:00
+    var x = 5
+    val y = 3.0
+    val result = x + y
+
+    println("x + y = ${x + y}")
+    println("x - y = ${x - y}")
+    println("x * y = ${x * y}")
+    println("x / y = ${x / y}")
+    println("x % y = ${x % y}")
+}

2360c21627ef433e5cbdaf9cbeffbc06fdd39020
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-02-02 10:01:38 -0400

    Add files via upload

--- 2024/ControlFlow/src/Functions.kt
+++ 2024/ControlFlow/src/Functions.kt
@@ -1,7 +1,20 @@
 fun main(args: Array<String>) {
-    val max = getMax(5, 9)
+    val max = getMax(3, 100, 10)
     println(max)
 }
 
 fun getMax(a: Int, b: Int) = if (a > b) a else b
     //return if (a > b) a else b
+
+fun getMax(a: Double, b: Double) = if (a > b) a else b
+
+fun getMax(a: Int, b: Int, c: Int): Int {
+    return if (a >= b && a >= c){
+        a
+    } else if (b >= a && b>= c){
+        b
+    } else {
+        c
+    }
+}
+

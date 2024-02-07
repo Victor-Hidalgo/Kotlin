7048ff255ef14732e36404d440af6a46038ea9ca
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-02-07 10:08:54 -0400

    Add files via upload

---
+++ 2024/ControlFlow/src/Loops.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+//    for (i in 1..10){
+//        println("i = $i")
+//    }
+//    for (i in 1 until 10){
+//        println(i)
+//    }
+
+//    for (i in 10 downTo 1){
+//        println(i)
+//    }
+
+//    for (i in 1 until 10 step 2) {
+//        println(i)
+//    }
+    for (i in 1..10) {
+        println(i)
+    }
+}

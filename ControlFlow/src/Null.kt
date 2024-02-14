1c155a41b7d3d90cfabb35118f10c500dbf24ff8
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-02-14 14:03:17 -0400

    Delete ControlFlow directory

--- ControlFlow/src/Null.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>){
-    var text: String? = null
-    text = "This variable is not null."
-    val text2: Any = text ?: "The variable is null."
-
-    println(text2)
-
-}

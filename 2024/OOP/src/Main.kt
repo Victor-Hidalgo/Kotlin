23f0cb31fa30dc370cea6e6b8519c138d4c8ff22
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-02-22 20:57:38 +0000

    Add files via upload

--- 2024/OOP/src/Main.kt
+++ 2024/OOP/src/Main.kt
@@ -9,6 +9,24 @@ fun main() {
     println("Model = ${car1.model}")
     println("Color = ${car1.color}")
     println("Doors = ${car1.doors}")
+
+    car1.move()
+    car1.stop()
+
+    val car2 = Car()
+    car2.name = "Ford"
+    car2.model = "Mustang"
+    car2.color = "Blue"
+    car2.doors = 2
+
+    println("\n")
+    println("Name = ${car2.name}")
+    println("Model = ${car2.model}")
+    println("Color = ${car2.color}")
+    println("Doors = ${car2.doors}")
+
+    car2.move()
+    car2.stop()
 }
 
 class Car {
@@ -18,10 +36,10 @@ class Car {
     var doors = 0
 
     fun move() {
-        println("The car is moving")
+        println("The car $name is moving")
     }
 
     fun stop() {
-        println("The car has stopped")
+        println("The car $name has stopped")
     }
 }

f59b8819e6bfcc82aa74f9de51b64c3b9dd6dab0
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-03-03 20:20:20 +0000

    Add files via upload

--- 2024/OOP/src/Classes.kt
+++ 2024/OOP/src/Classes.kt
@@ -8,6 +8,15 @@ class Car(name: String, var model: String, var color: String, var doors: Int) {
         println("The car $name has stopped")
     }
 }
+
 class User(var firstName: String, var lastName: String, var age: Int){
     lateinit var favoriteMovie: String
+}
+
+class Calculator() {
+    companion object {
+        fun sum(a: Int, b: Int): Int {
+            return a + b
+        }
+    }
 }
--- 2024/OOP/src/Main.kt
+++ 2024/OOP/src/Main.kt
@@ -1,13 +1,10 @@
 fun main() {
-    val result = Calculator.sum(5, 10)
-    val max = Int.MAX_VALUE
-    println(result)
+    println(Database)
+    println(Database)
 }
 
-class Calculator() {
-    companion object {
-        fun sum(a: Int, b: Int): Int {
-            return a + b
-        }
+object Database {
+    init {
+        println("Database created")
     }
 }

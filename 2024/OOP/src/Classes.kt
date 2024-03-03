f59780e54e54f5a7b0aabd1cd058341ccc4a35f0
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-03-03 20:28:28 +0000

    Add files via upload

--- 2024/OOP/src/Classes.kt
+++ 2024/OOP/src/Classes.kt
@@ -9,14 +9,16 @@ class Car(name: String, var model: String, var color: String, var doors: Int) {
     }
 }
 
-class User(var firstName: String, var lastName: String, var age: Int){
-    lateinit var favoriteMovie: String
-}
-
 class Calculator() {
     companion object {
         fun sum(a: Int, b: Int): Int {
             return a + b
         }
+    }
+}
+
+object Database {
+    init {
+        println("Database created")
     }
 }
--- 2024/OOP/src/Main.kt
+++ 2024/OOP/src/Main.kt
@@ -1,10 +1,14 @@
 fun main() {
-    println(Database)
-    println(Database)
+    val user1 = User("Emma", "Watson", 21)
+    val user2 by lazy {
+        User("Usuario", "Petro", 23)
+    }
+
+    println(user2.firstName)
 }
 
-object Database {
+class User(var firstName: String, var lastName: String, var age: Int){
     init {
-        println("Database created")
+        println("User: $firstName was created")
     }
 }
